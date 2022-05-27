b9b6e42cd1c36e5a9df3f226f07fde2b4f7e84aa
Pedro <Pedro@DESKTOP-3K4R30C>
2022-05-27 23:03:29 +0200

    commitLlibre

---
+++ GeneracioCodiPract2/src/Llibre.java
@@ -0,0 +1,68 @@
+public class Llibre {
+
+    private String nom;
+
+    private String tipus;
+
+    private String editorial;
+
+    private int any;
+
+    private Autor autor;
+
+    private String identificador;
+
+    private String estatBiblioteca;
+
+    public Llibre(String nom, String tipus, String editorial, int any, Autor autor, String identificador, String estatBiblioteca) {
+    }
+
+    public String getNom() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setNom(String nom) {
+    }
+
+    public String getTipus() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setTipus(String tipus) {
+    }
+
+    public String getEditorial() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setEditorial(String editorial) {
+    }
+
+    public int getAny() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setAny(int any) {
+    }
+
+    public Autor getAutor() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setAutor(Autor autor) {
+    }
+
+    public String getIdentificador() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setIdentificador(String identificador) {
+    }
+
+    public String getEstatBiblioteca() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setEstatBiblioteca(String estatBiblioteca) {
+    }
+}
