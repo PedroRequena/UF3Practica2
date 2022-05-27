2b0d50a5796ed0559fd977205ce955c66b6ac906
Pedro <Pedro@DESKTOP-3K4R30C>
2022-05-27 23:03:05 +0200

    commitLector

---
+++ GeneracioCodiPract2/src/Lector.java
@@ -0,0 +1,35 @@
+
+import java.util.List;
+
+public class Lector {
+
+    private List<Llibre> llibresPossesio;
+
+    private int quantitatLlibres;
+
+    private int quantitatMax;
+
+    public Lector(List<Llibre> llibresPossesio, int quantitatLlibres, int quantitatMax) {
+    }
+
+    public List<Llibre> getLlibresPossesio() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setLlibresPossesio(List<Llibre> llibresPossesio) {
+    }
+
+    public int getQuantitatLlibres() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setQuantitatLlibres(int quantitatLlibres) {
+    }
+
+    public int getQuantitatMax() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setQuantitatMax(int quantitatMax) {
+    }
+}
