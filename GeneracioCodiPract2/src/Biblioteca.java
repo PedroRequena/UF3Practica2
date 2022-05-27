608f5ca4433c91b3eee4a14ed0a05043b7406604
Pedro <Pedro@DESKTOP-3K4R30C>
2022-05-27 23:02:50 +0200

    commitBiblioteca

---
+++ GeneracioCodiPract2/src/Biblioteca.java
@@ -0,0 +1,17 @@
+
+import java.util.List;
+
+public class Biblioteca {
+
+    private List<Llibre> copiesLlibres;
+
+    public Biblioteca() {
+    }
+
+    public List<Llibre> getCopiesLlibre() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void setCopiesLlibre(List<Llibre> copiesLlibre) {
+    }
+}
